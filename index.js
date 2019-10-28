f812185ef6b6e9bba67e129548b4c539ff88342c
John Karrell Speck <speckworks@gmail.com>
2019-10-28 01:51:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let friends = [ 'Lisa', 'Kaitlin', 'Jan' ];
 function writeCards(friends, 'surprise' ){
   for(i = 0, i < friends.length, i++){
-    Console.log("Thank you,`friends[i]`, for the wonderful birthday gift!"
+    Console.log("Thank you,`friends[i]`, for the wonderful surprise gift!"
     )}}
