7e8df7eb4deeed0aa98081d58f7e1289335ca249
John Karrell Speck <speckworks@gmail.com>
2019-10-28 02:02:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function writeCards(friends, event){
   let thankYouCards = [];
   for(let i = 0, i < friends.length, i++){
+    thankYouCards.push
     Console.log(`Thank you,${friends[i]}, for the wonderful surprise gift!`
     );
   }}
