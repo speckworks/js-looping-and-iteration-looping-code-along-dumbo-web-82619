12a66550095d8650a0fcbff7622a75b1ee21cae5
John Karrell Speck <speckworks@gmail.com>
2019-10-28 02:16:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(friendsArray, event) {
+  let thankYouCards = []
+  for ( let i = 0; i < friendsArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${friendsArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+function countDown(i) {
+  while ( i > 0 ) {
+    console.log( i );
+    i -= 1;
+  }
+  console.log( i );
+}
