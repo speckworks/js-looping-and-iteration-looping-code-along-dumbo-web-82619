011cb00e77bd22d0640a419e6e8b3bbb05a69daf
John Karrell Speck <speckworks@gmail.com>
2019-10-28 01:45:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-// Code your solutions in this file
-// In `index.js`, build a function named `writeCards()` that accepts two arguments:
-// an array of string names, and an event name. Create a `for` loop with a counter
-// that starts at `0` and increments at the end of each loop. The condition should
-// halt the `for` loop after the last name in the array is printed out in the loop
-// body.
 
-// Inside the loop, create a custom message for each name from the provided array,
-// thanking that person for their gift. Collect the messages in an array and
-// return this array. For example:
-
-// ```js
-// writeCards(["Ada", "Brendan", "Ali"], "birthday");
-// ```
-
-// Would produce the following array:
-
-// ```js
-// [
-//   "Thank you, Ada, for the wonderful birthday gift!",
-//   "Thank you, Brendan, for the wonderful birthday gift!",
-//   "Thank you, Ali, for the wonderful birthday gift!"
-// ];
-// ```
 let friends = ["Ada", "Brendan", "Ali"];
 function writeCards(friends){
   for(i = 0, i < friends.length, i++)
