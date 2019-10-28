b363108d3d2f1b206086829e7172c1c26e9f1be8
John Karrell Speck <speckworks@gmail.com>
2019-10-28 01:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 let friends = [ 'Lisa', 'Kaitlin', 'Jan' ];
-function writeCards(friends, 'surprise' ){
+function writeCards(friends,'surprise'){
   for(i = 0, i < friends.length, i++){
     Console.log("Thank you,`friends[i]`, for the wonderful surprise gift!"
     )}}
