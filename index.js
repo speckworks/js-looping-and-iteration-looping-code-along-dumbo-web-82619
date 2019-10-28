4eb2f7cf4465dcd961504d85c34dea349a65a03d
John Karrell Speck <speckworks@gmail.com>
2019-10-28 02:04:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function writeCards(friends, event){
   let thankYouCards = [];
-  for(let i = 0, i < friends.length, i++){
+  for(let i = 0; i < friends.length; i++){
     thankYouCards.push(`Thank you,${friends[i]}, for the wonderful ${event} gift!`)
   }
   return thankYouCards
